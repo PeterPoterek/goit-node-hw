120639f49b0ba6331d5d3db7882a237cc09e840a
Piotr Poterek <peterpoterek0@gmail.com>
2024-04-06 23:08:36 +0200

    moongose setup wip

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
+const Contact = require("../../models/contacts.js");
+
 router.get("/", async (req, res, next) => {
-  res.json({ message: "Hello from api " });
+  try {
+    const contacts = await Contact.find({});
+
+    console.log(Contact);
+
+    res.json(contacts);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: `${err}` });
+  }
 });
 
 router.get("/:contactId", async (req, res, next) => {});
