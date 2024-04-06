6f423ba0be4f372827c24cd5e29d0927b08bb900
Piotr Poterek <peterpoterek0@gmail.com>
2024-04-06 23:21:22 +0200

    Update contacts.js

--- models/contacts.js
+++ models/contacts.js
@@ -4,21 +4,13 @@ const contactSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: [true, "Name is required"],
-      minLength: 2,
-      maxLength: 80,
+      required: [true, "Set name for contact"],
     },
     email: {
       type: String,
-      required: [true, "Email is required"],
-      minLength: 2,
-      maxLength: 100,
     },
     phone: {
       type: String,
-      required: [true, "Phone number is required"],
-      minLength: 2,
-      maxLength: 40,
     },
     favorite: {
       type: Boolean,
