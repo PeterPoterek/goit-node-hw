74c5926801ab0de5fff062652f7bcc6ec51ca5c2
Piotr Poterek <peterpoterek0@gmail.com>
2024-04-06 20:59:17 +0200

    validation.js moved into seperate file

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,16 +1,8 @@
 const express = require("express");
 const router = express.Router();
-
 const { v4 } = require("uuid");
-const Joi = require("joi");
-
 const { listContacts, getContactById, removeContact, addContact, updateContact } = require("../../models/contacts.js");
-
-const contactSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().email().required(),
-  phone: Joi.string().required(),
-});
+const { contactSchema } = require("../../validation.js");
 
 router.get("/", async (req, res, next) => {
   try {
@@ -43,7 +35,6 @@ router.post("/", async (req, res, next) => {
     }
 
     const id = v4().replace(/-/g, "").substring(0, 20);
-
     const newContact = { id, ...req.body };
 
     await addContact(newContact);
@@ -74,7 +65,6 @@ router.put("/:contactId", async (req, res, next) => {
   const contactId = req.params.contactId;
   try {
     const { error } = contactSchema.validate(req.body);
-
     if (error) {
       return res.status(400).json({ message: "Missing fields" });
     }
